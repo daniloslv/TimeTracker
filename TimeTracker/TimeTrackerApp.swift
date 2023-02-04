d30bed7f4e0338d5b0d8525b8174c849ffab6f15
Danilo Souza <souza.danilo@gmail.com>
2023-02-04 13:58:29 -0300

    Initial Commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Danilo Souza on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
